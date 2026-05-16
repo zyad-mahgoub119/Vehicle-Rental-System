e77fd9b65b5a9d91741f81b226b0c66c6f643a42
Zyad Mahgoub <setimoulaic119@gmail.com>
2026-05-16 17:25:44 +0300

    Create Car.h

---
+++ Source Code/Models/Car.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using namespace std;
+#include <string>
+
+struct Car {
+    int id;
+    string brand;
+    string model;
+    int year;
+    float pricePerDay;
+    string category;
+    bool available;
+};
